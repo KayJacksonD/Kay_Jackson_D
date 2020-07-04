45a5fdc53ca2184c29dc2793592433761ecf47d9
Kay <Kay.com>
2020-07-05 02:00:17 +0800

    07050200

--- jackson_parent/jackson_service_provider/src/main/java/com/it/jackson0419/vo/ApplicationVO.java
+++ jackson_parent/jackson_service_provider/src/main/java/com/it/jackson0419/vo/ApplicationVO.java
@@ -13,5 +13,4 @@ public class ApplicationVO {
     private String secondApprove;
     private String secondApproveDate;
     private String secondContent;
-    private String name;//必须属性，不然点击一个选项其他全部展开
 }
